b6dbd94316b8edc2b66c6507d908a1be09d89ffc
Amyra White <amyraawhite@cox.net>
2026-05-25 11:03:19 -0700

    Pipeline fix for Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from .database import engine
 from .models import Base
 
-# Creates all tables specified in models.py if they don't exist
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    # Creates all tables specified in models.py if they don't exist
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root(): 
